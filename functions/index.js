4950dbfc74aff2922c7aa7ff534e7518f8f70700
Willem Handreck <willem.handreck@gmail.com>
2022-06-23 22:50:38 +1000

    WIP

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,29 @@
-const functions = require("firebase-functions");
+const cors = require('cors')
+const express = require("express")
+const fetch = require("node-fetch")
+const functions = require("firebase-functions")
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// import cors from 'cors'
+// import express from 'express'
+// import fetch from 'node-fetch'
+// import functions from 'firebase-functions'
+
+const app = express()
+
+app.use(cors({ origin: true }))
+
+
+app.post('/', (request, response) => {
+  functions.logger.info(`New RSVP: ${request.body.name}`)
+  // response.send(request.body.name)
+  fetch('https://maker.ifttt.com/trigger/rsvp/with/key/c01X5W-6J65sXhMSX6e8Rg', {
+    method: 'post',
+    body: JSON.stringify(request.body),
+    headers: {'Content-Type': 'application/json'}
+  })
+  .catch(functions.logger.error)
+  response.end()
+})
+
+exports.rsvp = functions.https.onRequest(app)
+// const rsvp = functions.https.onRequest(app)
