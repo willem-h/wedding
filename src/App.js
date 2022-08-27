7617cab0d033f1c053b9f3b6cdea941f93ae03e5
Willem Handreck <willem-h@users.noreply.github.com>
2022-08-27 23:18:59 +1000

    Remove map iframe and just use an image with a link (#18)
    * Remove map iframe and just use an image with a link
    
    * Add alt prop to image
    
    * Add map link to body copy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
-import logo_white from './logo_white.svg';
+import logo_white from './logo_white.svg'
+import parking_map from './parking_map.png'
 import semi_circle from './semi_circle.svg'
 import './App.css';
 
@@ -173,10 +174,11 @@ function App() {
               <h1 className="font-serif text-4xl">PARKING</h1>
               <p>
                 Our venue has very limited parking, so in respect of neighbours,
-                parking is in the New Life Christian Church carpark, which is
-                located at Nambour Christian College. As soon as you drive in, you
-                will see the church carpark and our Event Bus Driver Richy - he will
-                be waiting to shuttle you to Bella Grace Manor. The shuttle will run
+                parking is in the <a href="https://goo.gl/maps/jE21atMj63S2Sp6X9" className="text-orange-600 underline underline-offset-4 hover:underline-offset-2">
+                New Life Christian Church carpark</a>, which is located at
+                Nambour Christian College. As soon as you drive in, you will see
+                the church carpark and our Event Bus Driver Richy - he will be
+                waiting to shuttle you to Bella Grace Manor. The shuttle will run
                 every 5-10mins, so you won’t be waiting long at all! Richy will be
                 there from 9.10am to 9.45am, ready for you. 
               </p>
@@ -187,18 +189,18 @@ function App() {
                 veranda to take you back to your car.
               </p>
 
-              <iframe
-                className="mx-auto"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d545.5797085599108!2d152.96043136104424!3d-26.644925908098834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b93788b628be79d%3A0x819627aef9b3b4f4!2sNEWLiFE%20Church%20Nambour!5e0!3m2!1sen!2sau!4v1661602372550!5m2!1sen!2sau"
-                width="600"
-                height="450"
-                style={{"border":0}}
-                allowFullScreen=""
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-                title="Parking Map"
-              >
-              </iframe>
+              <p>
+                <a href="https://goo.gl/maps/jE21atMj63S2Sp6X9">
+                  <img
+                    src={parking_map}
+                    className="w-11/12 max-w-screen-md mx-auto"
+                    alt="
+                      Map showing relative location of event parking to the
+                      greater Sunshine Coast area
+                    "
+                  />
+                </a>
+              </p>
             </div>
 
             <div className="space-y-6">
